84cad8510a832d9198319323f1b840b108c71edb
Amar Yasir <3marofficial249@gmail.com>
2025-02-04 08:44:34 +0200

    creating models and views

---
+++ backend/lemonapi/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from .models import Menu, Booking
+
+
+class MenuItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Menu
+        fields = "__all__"
+
+
+class BookingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Booking
+        fields = ["name", "email", "mobile", "date", "time", "guests"]
+
+    def validate_guests(self, value):
+        if value > 10:
+            raise serializers.ValidationError("Guests should be less that 10 person")
+        return value
+
+    def validate_time(self, value):
+        if Booking.objects.filter(time=value).exists():
+            raise serializers.ValidationError(
+                "This time is already booked, choose another tiem."
+            )
+        return value
